6306ab1e9193c1c83d25a2d01f2724ea29ba83c8
Tsung Hsuan <TsungHsuan@tomofun.com>
2022-09-13 01:35:56 +0800

    [Feature] 1. Finish the UI of search screen

---
+++ lib/ui/common/common_util.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+const smallColDivider = SizedBox(height: 10);
+const mediumColDivider = SizedBox(height: 20);
+const largeColDivider = SizedBox(height: 30);
+
+const smallRowDivider = SizedBox(width: 10);
+const mediumRowDivider = SizedBox(width: 20);
+const largeRowDivider = SizedBox(width: 30);
--- lib/ui/search/search_screen.dart
+++ lib/ui/search/search_screen.dart
@@ -1,4 +1,11 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+import '../common/common_util.dart';
+
+const String tag = "SearchScreen";
 
 class SearchScreen extends StatelessWidget {
   const SearchScreen({super.key, required this.showNavBottomBar});
@@ -7,12 +14,113 @@ class SearchScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme
+        .apply(displayColor: Theme.of(context).colorScheme.onSurface);
+
     return Expanded(
-        child: ListView(
-          children: [
-            Text("AAA"),
-          ],
+        child: Container(
+          padding: const EdgeInsets.all(20.0),
+          alignment: Alignment.center,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                  AppLocalizations.of(context)?.searchScanQrCodeHintText ?? "",
+                  style: textTheme.headlineSmall!,
+                  textAlign: TextAlign.center
+              ),
+              smallColDivider,
+              ElevatedButton.icon(
+                style: ElevatedButton.styleFrom(
+                  // ignore: prefer_const_constructors
+                  minimumSize: Size.fromHeight(70),
+                  // Foreground color
+                  // ignore: deprecated_member_use
+                  onPrimary: Theme.of(context).colorScheme.onPrimary,
+                  // Background color
+                  // ignore: deprecated_member_use
+                  primary: Theme.of(context).colorScheme.primary,
+                ).copyWith(
+                    elevation: ButtonStyleButton.allOrNull(0.0),
+                    // shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                    //     RoundedRectangleBorder(
+                    //         borderRadius: BorderRadius.circular(18.0),
+                    //         side: BorderSide(color: Colors.red)
+                    //     )
+                    // )
+                ),
+                onPressed: onSearchLocationBtnClick(context),
+                label: Container(
+                  width: double.infinity,
+                  padding: const EdgeInsets.only(left: 10.0, top: 10.0, right: 16.0, bottom: 10.0),
+                  child: Text(
+                    AppLocalizations.of(context)?.searchScanQrCodeBtnText ?? "",
+                    style: textTheme.titleLarge!.copyWith(
+                        color: Colors.white
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                icon: Container(
+                  width: 50,
+                  height: 50,
+                  padding: const EdgeInsets.only(left: 0.0),
+                  child: const Icon(Icons.qr_code),
+                ),
+              ),
+              smallColDivider,
+              Text(
+                  AppLocalizations.of(context)?.searchScanQrCodeHintText2 ?? "",
+                  style: textTheme.headlineSmall!,
+                  textAlign: TextAlign.center
+              ),
+              smallColDivider,
+              ElevatedButton.icon(
+                style: ElevatedButton.styleFrom(
+                  // ignore: prefer_const_constructors
+                  minimumSize: Size.fromHeight(70),
+                  // Foreground color
+                  // ignore: deprecated_member_use
+                  onPrimary: Theme.of(context).colorScheme.onPrimary,
+                  // Background color
+                  // ignore: deprecated_member_use
+                  primary: Theme.of(context).colorScheme.primary,
+                ).copyWith(elevation: ButtonStyleButton.allOrNull(0.0)),
+                onPressed: onSearchCommodityBtnClick(context),
+                label: Container(
+                  width: double.infinity,
+                  padding: const EdgeInsets.only(left: 10.0, top: 10.0, right: 16.0, bottom: 10.0),
+                  child: Text(
+                    AppLocalizations.of(context)?.searchScanQrCodeBtnText2 ?? "",
+                    style: textTheme.titleLarge!.copyWith(
+                        color: Colors.white
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                icon: Container(
+                  width: 50,
+                  height: 50,
+                  padding: const EdgeInsets.only(left: 0.0),
+                  child: const Icon(Icons.qr_code),
+                ),
+              )
+            ],
+          ),
         )
     );
+  }
+
+  void Function() onSearchLocationBtnClick(BuildContext context) {
+    return () {
+      debugPrint("$tag onSearchLocationBtnClick()");
+    };
+  }
+
+  void Function() onSearchCommodityBtnClick(BuildContext context) {
+    return () {
+      debugPrint("$tag onSearchCommodityBtnClick()");
+    };
   }
 }
